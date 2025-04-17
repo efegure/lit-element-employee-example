7514699b17d5bdb164649d111d844c2c54469ad2
efegure <efe__gure@hotmail.com>
2025-04-18 01:34:54 +0300

    feat:added more tests

--- test/employee-list-page_test.js
+++ test/employee-list-page_test.js
@@ -55,4 +55,142 @@ suite('employee-list-page', () => {
     `
     );
   });
+  test('render as list', async () => {
+    document.documentElement.lang = 'en';
+    const el = await fixture(
+      html` <employee-list-page .isList=${true}></employee-list-page> `
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <div id="employee-list-page">
+              <div id="employee-list-page-header">
+                <span id="employee-list-page-title">${i18n(
+                  'employeeList'
+                )}</span>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  
+                />
+                <div class="icon-container">
+                  <div >
+                    <iconify-icon
+                      class="icon active"
+                      icon="feather:grid"
+                    ></iconify-icon>
+                  </div>
+                  <div >
+                    <iconify-icon
+                      class="icon "
+                      icon="feather:list"
+                    ></iconify-icon>
+                  </div>
+                </div>
+              </div>
+              <employee-list
+                  >
+                    <pagination-selector></pagination-selector>
+                  </employee-list>
+            </div>
+            
+    `
+    );
+  });
+  test('render edit modal', async () => {
+    document.documentElement.lang = 'en';
+    const el = await fixture(
+      html`
+        <employee-list-page
+          .editPayload=${{}}
+          .showEditModal=${true}
+        ></employee-list-page>
+      `
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <div id="employee-list-page">
+              <div id="employee-list-page-header">
+                <span id="employee-list-page-title">${i18n(
+                  'employeeList'
+                )}</span>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  
+                />
+                <div class="icon-container">
+                  <div >
+                    <iconify-icon
+                      class="icon "
+                      icon="feather:grid"
+                    ></iconify-icon>
+                  </div>
+                  <div >
+                    <iconify-icon
+                      class="icon active"
+                      icon="feather:list"
+                    ></iconify-icon>
+                  </div>
+                </div>
+              </div>
+              <employee-table
+                  >
+                    <pagination-selector></pagination-selector>
+                  </employee-table>
+            </div>
+            <edit-modal index=""
+          ></edit-modal>
+    `
+    );
+  });
+  test('render delete modal', async () => {
+    document.documentElement.lang = 'en';
+    const el = await fixture(
+      html`
+        <employee-list-page
+          .deletePayload=${{}}
+          .showDeleteModal=${true}
+        ></employee-list-page>
+      `
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <div id="employee-list-page">
+              <div id="employee-list-page-header">
+                <span id="employee-list-page-title">${i18n(
+                  'employeeList'
+                )}</span>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  
+                />
+                <div class="icon-container">
+                  <div >
+                    <iconify-icon
+                      class="icon "
+                      icon="feather:grid"
+                    ></iconify-icon>
+                  </div>
+                  <div >
+                    <iconify-icon
+                      class="icon active"
+                      icon="feather:list"
+                    ></iconify-icon>
+                  </div>
+                </div>
+              </div>
+              <employee-table
+                  >
+                    <pagination-selector></pagination-selector>
+                  </employee-table>
+            </div>
+            <delete-modal
+          ></delete-modal>
+    `
+    );
+  });
 });
